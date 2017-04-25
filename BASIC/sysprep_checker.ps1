98cbf97c5c02eaab0474db3ee43c25fa47fd242e
tach80 <tach980@gmail.com>
2017-04-25 10:44:51 +0200

    Added another key
    Updated to correct some minor errors, added another registry key check and each check also provides date by itself. Also, some variable cleaning is added at the end.

--- BASIC/sysprep_checker.ps1
+++ BASIC/sysprep_checker.ps1
@@ -5,34 +5,50 @@
 
 # Variables and routes.
 $root="C:\Windows\System32\Setupcl.exe"
-$regkey="HKLM:\SYSTEM\Setup"
+$regkey1="HKLM:\SYSTEM\Setup"
+$regkey2="HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State"
+$auxjumper=0 # This variable is used to jump out of the switch.
 
 # CHAPTER ONE: a bit of on-screen text.
 
-Write-Host "Welcome $USUARIO"
+Write-Host "Welcome $USER"
 Write-Host "Let's see if your computer is based on a disk image,"
 Write-Host "so it's sysprep-ed, or it's a clean install."
 
 # CHAPTER TWO: code to look for cloning traces.
 
-for ($i=1; $i -le 4;$i++) {
-    switch ($i) {
-        "1" {if (Get-ItemProperty -Path "$regkey" -Name "CloneTag") {
-                $DATE=((Get-ItemProperty -Path "$regkey" -Name "CloneTag").CloneTag)
-                Write-Host "Found cloning key."    
-                Write-Host "Cloning creation date was $DATE"      
+for ($i=1; $i -le 5;$i++) {
+    if ($auxjumper -eq 0) { # To jump out of the switch.
+        switch ($i) {
+            "1" {if ((Get-ItemProperty -Path "$regkey1" -Name "CmdLine" -ErrorAction SilentlyContinue).CmdLine -match "Setup -newsetup -mini") {
+                    $DATE=((Get-ItemProperty -Path "$regkey1" -Name "CloneTag").CloneTag)
+                    Write-Host "Found cloning key."
+                    Write-Host "Cloning creation date was $DATE"
+                    $auxjumper++
                 }
             }
-        "2" {if ((Get-ItemProperty -Path "$regkey" -Name "CmdLine").CmdLine -match "Setup -newsetup -mini") {
-                Write-Host "Found cloning key."
-                Write-Host "Can't say cloning date." # For now.
+            "2" {if ((Get-ItemProperty -Path "$regkey1" -Name "SetupOemDuplicatorString" -ErrorAction SilentlyContinue)) {
+                    $DATE=((Get-ItemProperty -Path "$regkey1" -Name "CloneTag").CloneTag)
+                    Write-Host "Found cloning key."
+                    Write-Host "Cloning creation date was $DATE"
+                    $auxjumper++
                 }
             }
-        "3" {if (Get-Item $root) {
-                Write-Host "Cloning reference found."
+            "3" {if ((Get-ItemProperty -Path "$regkey2" -Name "ImageState" -ErrorAction SilentlyContinue).ImageState -match "IMAGE_STATE_COMPLETE") {
+                    $DATE=((Get-ItemProperty -Path "$regkey1" -Name "CloneTag").CloneTag) # To get the date.
+                    Write-Host "Found cloning key."
+                    Write-Host "Cloning creation date was $DATE" # Need to check this.
+                    $auxjumper++
+                }
             }
+            "4" {if (Get-Item $root) {
+                    Write-Host "Cloning reference found."
+                    Write-Host "Sorry, I can't say more."
+                    $auxjumper++
+                }
+            }
+            default {Write-Host "No cloning traces found. Seems a clean install."}
         }
-        default {Write-Host "No cloning traces found. Seems a clean install."}
     }
 }
 
